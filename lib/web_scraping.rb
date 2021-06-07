9dd0b1a5101ee3b097f8d943fc18610b515f4ff6
linqueta <lincolnrodrs@gmail.com>
2021-06-07 20:35:43 -0300

    parse data with nokogiri

--- lib/web_scraping.rb
+++ lib/web_scraping.rb
@@ -1,6 +1,16 @@
 # frozen_string_literal: true
 
 require 'web_scraping/version'
+require 'faraday'
+require 'nokogiri'
 
 module WebScraping
+  module_function
+
+  def run
+    url = "https://www.alleximoveis.com.br/imovel/679530-apartamento-para-locacao-no-cidade-nobre?operacao=LA&referencia=294"
+    response = Faraday.get(url)
+    binding.pry
+    url
+  end
 end
